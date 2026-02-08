62ef6859aa782c57b1b92073f053c441981308dc
kittyyykattt <153014215+kittyyykattt@users.noreply.github.com>
2026-02-08 00:59:43 -0500

    ss

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
+import { Router } from "wouter";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <Router base="/Nole-Path">
+    <App />
+  </Router>,
+);
+
